8dc89948e4a33ba9a788958d9e82968c81e22008
student <joycemumbiiwanjiku@gmail.com>
2023-05-05 20:48:28 +0300

    first commit

---
+++ secoquestions.py
@@ -0,0 +1,30 @@
+
+# 1. Write a Python program to get a single string from two given strings, separated by a space, and swap the first two characters of each string
+string1=("enter first string")
+string2=("enter second string")
+x=string1[0:2]
+string1=string2.replace(string1[0:2],string2[0:2])
+string2=string2.replace(string2[0:2],x)
+
+print("first string has become;-string2")
+# 2.  Write a Python function that takes a list of words and returns the longest word and the length of the longest one
+
+# 3. Write a Python program that accepts a comma-separated sequence of words as input and prints the distinct words in sorted form (alphanumerically).
+
+# 4.. Write a Python function that takes two lists and returns True if they have at least one common member.
+
+# 5. Write a Python program to convert a list to a list of dictionaries.
+# Sample lists: ["Black", "Red", "Maroon", "Yellow"], ["#000000", "#FF0000", "#800000", "#FFFF00"]
+
+# 6. Write a Python program to check whether all dictionaries in a list are empty or not
+
+# 7. Given a list of numbers, use list comprehension to remove all odd numbers from the list:
+# numbers = [3,5,45,97,32,22,10,19,39,43]
+
+# 8. Find the common numbers in two lists (without using a tuple or set) 
+# list_a = 1, 2, 3, 4, 
+# list_b = 2, 3, 4, 5 
+
+# 9. Use a nested list comprehension to find all of the numbers from 1-1000 that are divisible by any single digit besides 1 (2-9)
+
+# 10. Write a Python function to remove all vowels in a string
